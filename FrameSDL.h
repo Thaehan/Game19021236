029531bd5d8231c38d595bd7a53cb82e7f81316a
Dat Do <yasuonhe@gmail.com>
2020-04-27 03:11:55 +0700

    Let get it

--- FrameSDL.h
+++ FrameSDL.h
@@ -1,21 +1,11 @@
 #ifndef DEBUG
 #define DEBUG
 
+#include "MainObj.h"
 #include <iostream>
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <Windows.h>
-
-const int SCREEN_WIDTH = 600;
-const int SCREEN_HEIGHT = 600;
-const std::string WINDOW_TITLE = "Simple Snake by Do Van Dat";
-
-const std::string bgFile = "background.jpg";
-const std::string fontFile = "font.ttf";
-const int fontSize = 20;
-
-const int STEP_DELAY = 70;
-const int boxSize = 30;
 
 //This function will show the Error: standard output - Name of error - check funtion
 void logSDLError(std::ostream& os, const std::string& msg, bool fatal = false);
@@ -24,7 +14,7 @@ void logSDLError(std::ostream& os, const std::string& msg, bool fatal = false);
 void initSDL(SDL_Window*& window, SDL_Renderer*& renderer, int SCREEN_WITDH, int SCREEN_HEIGHT, const std::string& WINDOW_TITLE);
 
 //quit SDL
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer, SDL_Texture* bgImage);
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
 
 //Wait for a click or pressed key
 void waitKeyPressed();
